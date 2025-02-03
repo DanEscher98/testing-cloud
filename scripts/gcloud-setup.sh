92ed8a40593e31706b94a56e2c53cf8162ea40b3
Danyiel Colin <danyiel5978@gmail.com>
2025-02-03 04:22:29 -0600

    modified:   .github/workflows/gcp_deploy.yaml 	modified:   .gitignore 	modified:   scripts/gcloud-setup.sh

--- scripts/gcloud-setup.sh
+++ scripts/gcloud-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-set -e  # Exit on error
+#set -e  # Exit on error
 #trap 'rm -f ga_key.json' EXIT  # Delete key after script
 
 PROJECT_ID=$1
@@ -24,5 +24,5 @@ done
 gcloud iam service-accounts keys create ga_key.json \
   --iam-account="$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com"
 
-gh variable set GCP_PROJECT_ID=$PROJECT_ID
+gh variable set GCP_PROJECT_ID -b $PROJECT_ID
 gh secret set GCP_SA_KEY -a actions < ga_key.json
